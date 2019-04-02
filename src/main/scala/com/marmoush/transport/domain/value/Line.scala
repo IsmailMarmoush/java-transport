0d779b38b8f017ad8b500a28200df42bb6d8cad8
Ismail Marmoush <marmoushismail@gmail.com>
2019-04-03 01:41:35 +0200

    add another test

--- src/main/scala/com/marmoush/transport/domain/value/Line.scala
+++ src/main/scala/com/marmoush/transport/domain/value/Line.scala
@@ -3,6 +3,8 @@ package com.marmoush.transport.domain.value
 
 sealed abstract class Line(val name: String)
 
+case class AnonymousLine(override val name: String) extends Line(name)
+
 case object LineM4 extends Line("M4")
 
 case object Line200 extends Line("200")
--- src/test/scala/com/marmoush/domain/services/VehicleServiceTest.scala
+++ src/test/scala/com/marmoush/domain/services/VehicleServiceTest.scala
@@ -2,11 +2,15 @@ package com.marmoush.domain.services
 
 import org.scalatest.{FlatSpec, Matchers}
 import com.marmoush.transport.Dependencies.vehicleService._
-import com.marmoush.transport.domain.value.{Line, LineM4}
+import com.marmoush.transport.domain.value.{AnonymousLine, Line, LineM4}
 
 class VehicleServiceTest extends FlatSpec with Matchers {
   "Line M4" should "be delayed" in {
     isLineDelayed(LineM4) should be(true)
   }
-  
+
+  "Line U8" should "not be in delayed" in {
+    isLineDelayed(AnonymousLine("U8")) should be(false)
+  }
+
 }
