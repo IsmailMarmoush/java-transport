764db93ebea8a3871a65c8a94c21d96f7098f4ee
Ismail Marmoush <ismail.marmoush@zalando.de>
2019-04-07 14:17:01 +0200

    stopEntity

--- src/main/scala/com/marmoush/transport/adapter/repo/StopsCSVRepo.scala
+++ src/main/scala/com/marmoush/transport/adapter/repo/StopsCSVRepo.scala
@@ -3,6 +3,7 @@ package com.marmoush.transport.adapter.repo
 import com.github.tototoshi.csv.CSVReader
 import com.marmoush.transport.domain.entities.StopEntity
 import com.marmoush.transport.domain.repo.StopsRepo
+import com.marmoush.transport.domain.value.Stop
 
 import scala.io.Source
 
@@ -11,5 +12,7 @@ case class StopsCSVRepo(csvFile: Source) extends StopsRepo {
 
   override def stopId(x: Int, y: Int): Option[Int] =
     reader.toStream.find { case (id :: x1 :: y1) => x == x1 && y == y1 }.map(_.head.toInt)
-  override def stop(id: Int): Option[StopEntity] = ???
+  override def stop(id: Int): Option[StopEntity] = reader.toStream.find { case (idx :: x :: y) => idx == id }.map {
+    case (idx :: x :: y :: nil) => new StopEntity(idx.toInt, new Stop(x.toInt, y.toInt))
+  }
 }
